4d3154cb8e918e35b3eba63e36446cfcc3933d82
Anthony <anthonygavriel@gmail.com>
2024-04-03 10:40:48 +0100

    testing

--- script.py
+++ script.py
@@ -2,12 +2,21 @@ from agents.forager import Forager
 from agents.hunter import Hunter
 from agents.plant import Plant
 
-# Default difference between compatibility thresholds
-MAX_DIFF = 0.1
-
 f1 = Forager('f1', 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 'M')
 f2 = Forager('f2', 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 'F')
 
+shrub = Plant(name='shrub', sustenance_granted=0.8)
 
+f1.get_attributes()
+for i in range(3):
+    f1.hunger_increase()
+f1.get_attributes()
 
+print('-'*50+'\n')
+
+f2.get_attributes()
+f2.eat(shrub)
+f2.get_attributes()
+
+f1.die()
 f1.get_attributes()
