d49f098c770c3514494e3c098637f87d8054b959
Anthony <anthonygavriel@gmail.com>
2024-04-03 08:32:37 +0100

    remove sustenance value

--- agents/hunter.py
+++ agents/hunter.py
@@ -1,23 +1,10 @@
 from .mammal import Mammal
 
 class Hunter(Mammal):
-    def __init__(self, id, hunger, bravery, agility, 
-                 perception, strength, endurance):
+    def __init__(self, id: str, hunger: float, bravery: float, agility: float, 
+                 perception: float, strength: float, endurance: float,):
         super().__init__(id, hunger, bravery, agility, 
                          perception, strength, endurance)
-        
-        self.sustenance_granted = self.calculate_sustenance()
     
-    def calculate_sustenance(self) -> int:
-        """
-        Calculate how much sustenance the hunter gives the forgager if they lose the fight
-        """
-        pass  
-    
-    def sustenance(self) -> int:
-        try:
-            return self.sustenance_granted
-        except:
-            print(f'No sustenance value for Hunter {self.id}')
-        
-        
+    def __str__(self) -> str:
+        return f'Hunter {self.id}'
