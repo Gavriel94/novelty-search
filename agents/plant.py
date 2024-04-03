3c83e6bbc951c4fa8a036937dabe6991008f3ecb
Anthony <anthonygavriel@gmail.com>
2024-04-03 10:40:43 +0100

    just use attributes

--- agents/plant.py
+++ agents/plant.py
@@ -1,7 +1,5 @@
 class Plant():
-    def __init__(self, sustenance_granted: float):
+    def __init__(self, name: str, sustenance_granted: float):
+        self.name = name
         self.sustenance_granted = sustenance_granted
-    
-    def sustenance(self) -> float:
-        return self.sustenance_granted
         
