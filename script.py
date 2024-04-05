68a1e1b598e31afcc315cc274882755f900fad8b
Anthony <anthonygavriel@gmail.com>
2024-04-05 14:14:08 +0100

    hunger tests

--- script.py
+++ script.py
@@ -3,9 +3,22 @@ from agents.hunter import Hunter
 from agents.food import Food
 from agents.ravine import Ravine
 
-f1 = Forager(id='f1', hunger=3.0, bravery=3.0, agility=10.0, perception=3.0, strength=3.0, endurance=10.0, sex='M')
-f2 = Forager('f2', 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 'F')
-o = f1.produce_offspring(f2)
-snack = Food()
+f1 = Forager(sex='M')
 
-print(snack)
+f1.display_attributes()
+food = Food()
+print(food)
+
+for i in range(100):
+    f1.eat(food)
+    
+f1.display_attributes()
+
+print('\n' + '-' * 50 + '\n')
+
+f2 = Forager(sex='F')
+f2.display_attributes()
+
+for i in range(100):
+    f2.hunger_increase()
+f2.display_attributes()
