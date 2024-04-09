fdc4e74b080abcc9c2cc9d063b5a9937117d322c
Anthony <anthonygavriel@gmail.com>
2024-04-09 13:54:45 +0100

    tests

--- script.py
+++ script.py
@@ -1,24 +1,26 @@
-from agents.forager import Forager
-from agents.hunter import Hunter
-from agents.food import Food
-from agents.ravine import Ravine
+from simulation.agents.forager import Forager
+from simulation.agents.hunter import Hunter
+from simulation.agents.food import Food
+from simulation.agents.ravine import Ravine
+from simulation.environment.grid import Grid
 
+stuff = []
+r1 = Ravine()
+r2 = Ravine()
 f1 = Forager(sex='M')
-
-f1.display_attributes()
-food = Food()
-print(food)
-
-for i in range(100):
-    f1.eat(food)
-    
-f1.display_attributes()
-
-print('\n' + '-' * 50 + '\n')
-
 f2 = Forager(sex='F')
-f2.display_attributes()
+h1 = Hunter()
+h2 = Hunter()
+h3 = Hunter()
+as1 = Food()
+as2 = Food()
+as3 = Food()
 
-for i in range(100):
-    f2.hunger_increase()
-f2.display_attributes()
+stuff.extend([r1, r2, f1, f2, h1, h2, h3, as1, as2, as3])
+
+g = Grid(10, 10)
+g.setup_environment(stuff)
+
+# g.setup_environment(stuff)
+
+g.display_grid()
